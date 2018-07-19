ad4b68755f6cc4bd90173451c4a12cf2d51f54f9
SimranRiar <simriar@gmail.com>
2018-07-19 12:05:46 -0400

    4 - Upgraded Follow Camera With Perspective Lock
    Problem: Can't actually reverse, but this isn't that big of an issue. At least, not at the moment.

--- Get Milk!/Assets/Scripts/CameraController.cs
+++ Get Milk!/Assets/Scripts/CameraController.cs
@@ -37,17 +37,67 @@ public class CameraController : MonoBehaviour
     ***************************************************************************************/
 
 
-    public Transform target;
+    /*    public Transform target;
 
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+        public float smoothSpeed = 0.125f;
+        public Vector3 offset;
 
-    void FixedUpdate()
-    {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        void FixedUpdate()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
 
-        transform.LookAt(target);
+            transform.LookAt(target);
+        }
     }
+
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    private void Update()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }*/
+
 }
