afe8f244dae1b511c401f14adaa60f4126556f23
SimranRiar <simriar@gmail.com>
2018-07-19 11:37:32 -0400

    3 - Upgraded Follow Camera With Better Perspective

--- Get Milk!/Assets/Scripts/CameraController.cs
+++ Get Milk!/Assets/Scripts/CameraController.cs
@@ -13,19 +13,41 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject player;
+    /*public GameObject player;
     private Vector3 offset;
-
 
     // Use this for initialization
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+    }*/
+
+    /***************************************************************************************
+    *    Title: <CameraFollow>
+    *    Author: <Brackeys>
+    *    Source: YouTube Video
+    *    Availability: <https://www.youtube.com/watch?v=MFQhpwc6cKE>
+    *
+    ***************************************************************************************/
+
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
     }
 }
