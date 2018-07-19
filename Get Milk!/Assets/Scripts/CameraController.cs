4e968e3391e04ed1713a2940fb13d032db943ea7
SimranRiar <simriar@gmail.com>
2018-07-19 11:08:19 -0400

    2 - Rudimentary Follow Camera

---
+++ Get Milk!/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿/***************************************************************************************
+*    Title: <CameraController>
+*    Author: <Jeremy Gibson Bond>
+*    Code version: <Second Edition>
+*    Availability: <Introduction to Game Design, Prototyping, and Develeopment>
+*
+***************************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
