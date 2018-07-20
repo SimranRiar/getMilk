83f4f43d037fa82d447d182db6fc2083068ab11c
SimranRiar <simriar@gmail.com>
2018-07-19 23:53:54 -0400

    9 - Scenes Completed - Need Trigger Scene Changes

---
+++ Get Milk!/Assets/Scripts/Trigger_1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trigger_1 : MonoBehaviour
+{
+    void onTriggerEnter(Collider other)
+    {
+        Debug.Log("Passed through Trigger");
+    }
+}
